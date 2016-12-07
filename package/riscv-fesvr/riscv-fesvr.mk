9636c703dd17d6e73795b14b2c136f8dc8266c09
Xavier Roumegue <xroumegue@gmail.com>
2016-12-07 17:24:55 +0100

    riscv-fesvr: Add support for target
    Signed-off-by: Xavier Roumegue <xroumegue@gmail.com>

--- package/riscv-fesvr/riscv-fesvr.mk
+++ package/riscv-fesvr/riscv-fesvr.mk
@@ -8,3 +8,4 @@ RISCV_FESVR_VERSION = 7c56507ba1a4a9a2f83773590f3fd9f122871c05
 RISCV_FESVR_SITE = $(call github,riscv,riscv-fesvr,$(RISCV_FESVR_VERSION))
 
 $(eval $(host-autotools-package))
+$(eval $(autotools-package))
